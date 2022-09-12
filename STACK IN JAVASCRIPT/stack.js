47f7faa31e2bc94375d7e88b013751ea7d344b25
ISAAC UDOFIA <100437247+isaacudofia@users.noreply.github.com>
2022-09-12 20:05:46 +0100

    first commit

---
+++ STACK IN JAVASCRIPT/stack.js
@@ -0,0 +1,29 @@
+// STACK IN JAVASCRIPT
+
+const A = []
+
+
+A.push("Isaac")
+A.push("David")
+A.push("Paul")
+A.push("Josh")
+
+const index1 = A.indexOf("Josh") 
+const index2 = A.indexOf("Paul")
+
+console.log(index1);
+console.log(index2);
+
+const pop1 =A.pop()
+const pop2=A.pop()
+
+console.log(pop1);
+console.log(pop2);
+
+const index3 = A.indexOf(pop1) 
+const index4 = A.indexOf(pop2)
+
+console.log(index3);
+console.log(index4);
+
+console.log(A);
