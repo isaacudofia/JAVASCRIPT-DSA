5b4ef5e84e0366ccb66e200688dd2143f222e8b5
isaacudofia <isaacudofia0@gmail.com>
2022-10-17 22:12:51 +0100

    Commit Made

--- BUBBLE_SORT/bubbleSort.js
+++ BUBBLE_SORT/bubbleSort.js
@@ -2,20 +2,20 @@
 //The bubble sort has a space complexity of O(1).
 //This algorithm has a worst-case time complexity of O(n2)
 
-const A = [10, 12, 15, 30, 5]
+const A = [2, 9, 1, 6, 5, 4]
 
-for(let i = 0 ; i < A.length -1; i++){
-  for(let j = 0 ; j < A.length-1-i; j++){
-
-    console.log(`i,j : ${i} ${j} > ${j+1}: ${A[i]} ${A[j]}`)
-
-    if(A[j] > A[j+1]){
-        const temp = A[j]
-        console.log(temp);
-        A[j] = A[j+1]
-        A[j+1] = temp
-    }
-  }
+const bubbleSort=(A)=>{
+   for(let i = 0; i < A.length; i++){
+     for(let j = 0; j < A.length - i; j++){
+       let temp
+       if(A[j] > A[j+1]){
+         temp = A[j]
+         A[j] = A[j+1]
+         A[j+1] = temp
+       }
+     }
+   }
+   return A
 }
 
-console.log(A);
+console.log(bubbleSort(A))
