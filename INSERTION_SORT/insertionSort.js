67c48205ae7d112779f747bc2f2d21479e644443
isaacudofia <isaacudofia0@gmail.com>
2022-10-18 19:59:27 +0100

    Initial Commit

--- INSERTION_SORT/insertionSort.js
+++ INSERTION_SORT/insertionSort.js
@@ -1,9 +1,16 @@
-const array = [2, 9, 1, 6, 5, 4];
+const array = [9, 2, 6, 5, 4, 1];
 
 const insertionSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    console.log("heell");
+  for (let i = 1; i < array.length; i++) {
+    let temp = i;
+    let j = i - 1;
+    while (j >= 0 && array[temp] < array[j]) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = temp;
   }
+  return array;
 };
 
-insertionSort(array);
+console.log(insertionSort(array));
