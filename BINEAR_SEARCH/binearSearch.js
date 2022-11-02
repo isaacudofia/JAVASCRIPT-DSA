1fee44054e12f9ba3a03d4f191a0c31df1c6155f
ISAAC UDOFIA <100437247+isaacudofia@users.noreply.github.com>
2022-11-02 13:23:32 +0100

    Update binearSearch.js

--- BINEAR_SEARCH/binearSearch.js
+++ BINEAR_SEARCH/binearSearch.js
@@ -27,7 +27,6 @@ const binearSearch = (arr, search) => {
   return -1;
 };
 
-
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //ARRAY NEEDS TO BE SORTED FOR BINARY SEARCH RULE 1
 
 const findNumber = 9; //SEARCH VALUE HERE
